0f88a94fb561a8360150a0b0a160abda3cf539d5
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-04 17:14:37 -0700

    Set up files and requirement comments

---
+++ README.md
@@ -0,0 +1,26 @@
+# SBA 308A: JavaScript Web Application
+## Project description
+This project showcases my capability to implement advanced JavaScript tools and features in a practical manner.
+
+### Objectives
+- Use asynchronous JavaScript tools to build a responsive web application.
+- Demonstrate understanding of the JavaScript event loop.
+- Generate asynchronous code using Promises and async/await syntax.
+- Use fetch and/or Axios to interact with an external web API.
+- Organize files using modules and imports.
+
+## Technologies used
+- JavaScript
+- HTML
+- CSS
+
+## How to get started
+1. Clone the repo
+    - **git clone https://github.com/lmfmay/SBA-308A.git**
+
+2. Change git remote url to avoid accidental pushes to base project
+    - **git remote set-url origin github_username/repo_name**
+    - **git remote -v # confirm the changes**
+
+## Acknowledgements
+- 
---
+++ scripts/script.mjs
@@ -0,0 +1,11 @@
+// Use the fetch API or Axios to communicate with an external web API. Use the data provided by this API to populate your application’s content and features.
+// Create user interaction with the API through a search feature, paginated gallery, or similar. This feature should use GET requests to retrieve associated data.
+// Enable user manipulation of data within the API through the use of POST, PUT, or PATCH requests. Ensure your chosen API supports this feature before beginning.
+// Make use of Promises and async/await syntax as appropriate.
+// Organize your JavaScript code into at least three (3) different module files, and import functions and data across files as necessary.
+// Ensure the program runs as expected, without any undesired behavior caused by misunderstanding of the JavaScript event loop (such as race conditions, API calls being handled out of order, etc.).
+// Create an engaging user experience through the use of HTML and CSS.
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+// Commit frequently to the git repository.
+// Include a README file that contains a description of your application.
+// Level of effort displayed in creativity, presentation, and user experience 
